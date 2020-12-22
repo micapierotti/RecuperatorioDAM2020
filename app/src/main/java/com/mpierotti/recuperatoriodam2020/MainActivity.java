fa93b949521167f01dde2f4aeee3affb4cd30c65
micapierotti <micapierotti@live.com.ar>
2020-12-21 21:58:22 -0300

    commit 9:57 p.m.

--- app/src/main/java/com/mpierotti/recuperatoriodam2020/MainActivity.java
+++ app/src/main/java/com/mpierotti/recuperatoriodam2020/MainActivity.java
@@ -2,13 +2,41 @@ package com.mpierotti.recuperatoriodam2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt_mapa;
+    private EditText ciudad;
+    private Spinner spinner;
+    private List<String> resultado = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_mapa = findViewById(R.id.bt_mapa);
+        ciudad = findViewById(R.id.editTextCiudad);
+        spinner = findViewById(R.id.spinner);
+
+        resultado.add(ciudad.getText().toString());
+        resultado.add(spinner.getSelectedItem().toString());
+
+        bt_mapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MapaActivity.class);
+                i.putStringArrayListExtra("ciudad", (ArrayList<String>) resultado);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/mpierotti/recuperatoriodam2020/MapaActivity.java
+++ app/src/main/java/com/mpierotti/recuperatoriodam2020/MapaActivity.java
@@ -1,14 +1,107 @@
 package com.mpierotti.recuperatoriodam2020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MapaActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapaActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private ArrayList<String> ciudad;
+
+    List<LatLng> latLngList = new ArrayList<>();
+    List<Marker> markerList = new ArrayList<>();
+    Polyline polyline = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Intent in = getIntent();
+        Bundle b = in.getExtras();
+        if (b != null) {
+            ciudad = (ArrayList<String>) b.get("ciudad");
+        }
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        updateMapa();
+    }
+
+    public void updateMapa(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    9999);
+            return;
+        }
+        MarkerOptions ciudad_marker = new MarkerOptions()
+                .position(new LatLng(-31.7473137,-60.5499826))
+                .title(ciudad.get(0) +", "+ciudad.get(1))
+                .snippet(ciudad.get(0));
+
+        Marker marker = mMap.addMarker(ciudad_marker);
+        markerList.add(marker);
+        latLngList.add(marker.getPosition());
+
+        MarkerOptions ciudad_marker2 = new MarkerOptions()
+                .position(new LatLng(-31.6181236,-60.7762974))
+                .title("Otra ciudad");
+
+        Marker marker2 = mMap.addMarker(ciudad_marker2);
+        markerList.add(marker2);
+        latLngList.add(marker2.getPosition());
+
+        dibujarPolyline();
+    }
+    public void dibujarPolyline(){
+        if (polyline != null){
+            polyline.remove();
+        }
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .addAll(latLngList).clickable(true);
+        polyline = mMap.addPolyline(polylineOptions);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String
+            permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 9999: {
+                if (grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    updateMapa();
+                }
+            }
+
+        }
     }
 }
